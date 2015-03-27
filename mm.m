85d4045d71deb18c6cb5d22dbd9e645e00c0d257
unknown <s2882161@PC026803.staff.ad.griffith.edu.au>
2015-03-27 10:12:51 +1000

    add Morphology

---
+++ mm.m
@@ -0,0 +1,70 @@
+function feat = mm(dataCube)
+
+% mathematical morphological methods 
+% implement the extended morphological profiles method for hyperspectral image classfication 
+% reference - Fauvel, M.; Tarabalka, Y.; Benediktsson, J.A.; Chanussot, J.; Tilton, J.C., "Advances in Spectral-Spatial Classification of Hyperspectral Images," Proceedings of the IEEE , vol.101, no.3, pp.652,675, March 2013
+
+debug = false; % show figures and output variables
+
+% step 1 Extract the majority information and reduce band numbers
+dataCube = single(dataCube);
+[m, n, b] = size(dataCube);
+vdataCube = reshape(dataCube, [m*n, b]);
+
+% PCA
+numfPC = 3;
+[~, score,~] = princomp(vdataCube); 
+vdataCubePc = score(:, 1:numfPC);
+dataCubePc = reshape(vdataCubePc, [m, n, numfPC]);
+
+
+% step 2 Morphological process
+numofSe = 4;
+iniSizeofSe = 1; %initial size of Se ?????
+dimofMp = numfPC*(numofSe*2 + 1); % dimension of morphological profile
+emp = zeros(m, n, dimofMp); % EMP feature
+
+for indexofPc = 1:numfPC
+   pc = dataCubePc(:,:,indexofPc);
+   mid = (indexofPc-1)* (numofSe*2 + 1) + (numofSe + 1) ; 
+   emp(:,:, mid) = pc ;
+   for indexofSe = 1:numofSe
+      sizeofSe = iniSizeofSe + 2;
+      se = strel('disk', sizeofSe);
+      marker = imerode(pc,se); % opening by reconstruction
+      opening = imreconstruct(marker,pc);
+      emp(:,:, mid + indexofSe) = opening;
+      marker = imdilate(pc, se); % closing by reconstruction
+      temp = imreconstruct(imcomplement(marker), imcomplement(pc));
+      closing = imcomplement(temp);
+      emp(:,:, mid - indexofSe) = closing;
+   end
+end
+vemp = reshape(emp, [m*n,dimofMp]);
+emp = mapstd(vemp); % normalise
+feat = reshape(emp, [m, n, dimofMp]); 
+
+if debug == true
+   figure, 
+   for i = 1:size(emp,3)
+       slice = emp(:,:, i);
+       imshow(slice, []);
+       pause(1);
+   end
+   export2base;
+end
+
+% step 3 Feature fusion
+
+
+
+
+
+
+
+function export2base
+w = evalin('caller','who');
+n = length(w);
+for i = 1:n
+    assignin('base',w{i},evalin('caller',w{i}))
+end
